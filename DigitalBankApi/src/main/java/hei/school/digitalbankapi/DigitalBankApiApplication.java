f4273c7467c375fec4891f34df6b4c651b506919
RotsyMandrindra <rotsy.rasoanaivo@gmail.com>
2024-03-14 20:48:07 +0300

    feat : generate a spring project

---
+++ DigitalBankApi/src/main/java/hei/school/digitalbankapi/DigitalBankApiApplication.java
@@ -0,0 +1,13 @@
+package hei.school.digitalbankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBankApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalBankApiApplication.class, args);
+    }
+
+}
